2074fdc0519d46637a400dfd93dce78479498c50
subhanshu-pal <subhanshupal7@gmail.com>
2026-08-01 14:53:41 +0530

    update all

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+// console.log("Hello World");
+
+const math = require('./math.js'); // Importing the math.js module
+// require is a built-in function in Node.js that allows you to 
+// include and use external modules or files in your code. 
+// In this case, it is used to import the math.js module, 
+// which contains the add and subtract functions.
+
+// The ./ indicates that the math.js file is located in the same directory as the current file (hello.js).
+
+console.log("math value is ", math.add(2,3)); // Calling the add function from the math.js module
+console.log("math value is ", math.subtract(5,3)); // Calling the subtract function from the math.js module
--- math.js
+++ math.js
@@ -1,9 +1,14 @@
 function add(a, b) {
     return a+b;
 }
-console.log(add(2,3));
+// console.log(add(2,3));
 
 function subtract (a,b ) {
     return a-b;
 }
-console.log(subtract(5,3));
+// console.log(subtract(5,3));
+
+module.exports = {
+    add: add,
+    subtract: subtract
+};
