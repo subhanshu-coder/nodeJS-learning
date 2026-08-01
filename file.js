8b0fa9ef3ad020b9e6f57205651b5f31891fb1b7
subhanshu-pal <subhanshupal7@gmail.com>
2026-08-01 15:17:23 +0530

    update all

--- file.js
+++ file.js
@@ -7,3 +7,6 @@ fs.writeFileSync("./hello.txt", "Hello World!"); // Writing "Hello World!" to a 
 // The following code snippet demonstrates how to write to a file asynchronously using the fs.writeFile() method. This method takes three arguments: the file path, the data to be written, and a callback function that is executed once the write operation is complete. If an error occurs during the write operation, it will be passed to the callback function as an argument.
 fs.writeFile("./hello.txt", "hello world Async", (err) => {}); // Writing "hello world Async" to 'hello.txt' asynchronously
 
+fs.appendFileSync("./hello.txt", ` ${Date.now()} hey there\n`); //appending the current timestamp and "hey there" to 'hello.txt' synchronously
+ // Appending text to 'hello.txt' synchronously
+// The fs.appendFileSync() method is a synchronous operation that appends data to a file. If the file does not exist, it will be created. In this case, it appends " Appending this text to the file." to 'hello.txt'.
