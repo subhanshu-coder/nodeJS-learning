f58e771c1b876c51e678719d8877023dc22baf61
subhanshu-pal <subhanshupal7@gmail.com>
2026-08-01 16:17:04 +0530

    update all

---
+++ works.js
@@ -0,0 +1,11 @@
+// blocking...
+const fs = require("fs"); // Importing the 'fs' module to work with the file system
+
+console.log(1);
+const result = fs.readFileSync("hello2.txt", "utf-8"); // Writing "Hello World!" to a file named 'hello.txt' in the current directory
+console.log("result is ", result);
+console.log(2);
+
+// utf-8 is a character encoding that represents text in computers and other devices. 
+// It is a widely used encoding that can represent a wide range of characters from different languages and scripts.
+// In this case, it specifies that the content being written to the file should be encoded using the UTF-8 character encoding.
