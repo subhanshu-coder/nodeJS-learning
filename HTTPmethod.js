5aa5f317754ca38a11f44e3e18879306d44556cd
subhanshu-pal <subhanshupal7@gmail.com>
2026-08-02 02:09:23 +0530

    update all

---
+++ HTTPmethod.js
@@ -0,0 +1,8 @@
+// http method
+
+//  there are several HTTP methods that can be used to perform different actions on a server. The most common HTTP methods are:
+// 1. GET: The GET method is used to retrieve data from a server. It is the most commonly used HTTP method and is typically used to request web pages or other resources.
+// 2. POST: The POST method is used to send data to a server to create or update a resource. It is commonly used when submitting form data or uploading files.
+// 3. PUT: The PUT method is used to update an existing resource on the server. It is similar to the POST method, but it is idempotent, meaning that multiple identical requests will have the same effect as a single request.
+// 4. DELETE: The DELETE method is used to delete a resource from the server. It is typically used when a client wants to remove a specific resource.
+// 5. PATCH: The PATCH method is used to apply partial modifications to a resource. It is similar to the PUT method, but it allows for updating only specific fields of a resource rather than replacing the entire resource. 
