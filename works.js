efc25c27062a80671322ad067ba020e82ab39bae
subhanshu-pal <subhanshupal7@gmail.com>
2026-08-01 16:20:44 +0530

    update works.js

--- works.js
+++ works.js
@@ -9,3 +9,12 @@ console.log(2);
 // utf-8 is a character encoding that represents text in computers and other devices. 
 // It is a widely used encoding that can represent a wide range of characters from different languages and scripts.
 // In this case, it specifies that the content being written to the file should be encoded using the UTF-8 character encoding.
+
+// non-blocking...
+
+console.log(1);
+fs.readFile("hello2.txt", "utf-8", (err, result) => {
+    console.log("result is ", result);
+}); // Writing "Hello World!" to a file named 'hello.txt' in the current directory
+console.log(2);
+console.log(3);
